9d8d2af1585d42d9bee05d8937d97519e26b3631
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-17 12:43:38 +0530

    Homework

---
+++ Congratulations.py
@@ -0,0 +1,10 @@
+text = "ConGrAtulAtioNs"
+print(text.upper())
+print(text.lower())
+strOne = input("strOne is: ")
+strTwo = input("strTwo is: ")
+strThree = strOne + " " + strTwo
+print("By concatenation, strThree is: ",strThree)
+
+
+
