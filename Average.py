0f068bd42f0abbbdb541bf43bc0ac735b68ad073
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-19 13:12:39 +0530

    Average

---
+++ Average.py
@@ -0,0 +1,12 @@
+#Storing values
+tree1 = 12
+tree2 = 13
+tree3 = 14
+tree4 = 15
+tree5 = 16
+#Adding all the values
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of variables are: ", sum)
+#Finding average
+average = sum/5
+print("The average of variables are: ", average)
