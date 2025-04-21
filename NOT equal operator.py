a3ea13d033c18b5ce4405688c89e175ace040004
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-21 12:16:29 +0530

    NOT-equal operator

---
+++ NOT equal operator.py
@@ -0,0 +1,19 @@
+a = 15
+b = 10
+c = 2
+print(a!=b)
+print(b!=c)
+
+a = 'python'
+b = 'coding'
+if a!=b:
+    print(a, "is not equal to coding")
+a = 4
+b = 5
+if (a==1) and (b==5):
+    print("hello")
+i = int(input("Enter a no: "))
+if i%2!=0:
+    print("i is not an even number")
+else:
+    print("i is an odd number")
