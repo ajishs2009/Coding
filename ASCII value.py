da2bba7d3563c88af8052252c7f1229bc1e872f0
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-22 11:52:17 +0530

    ASCII value

---
+++ ASCII value.py
@@ -0,0 +1,55 @@
+x = int(input("Enter the ASCII number between 65-90: "))
+if x == 65:
+    print("ASCII code alphabet is A")
+elif x == 66:
+    print("ASCII code alphabet is B")
+elif x == 67:
+    print("ASCII code alphabet is C")
+elif x == 68:
+    print("ASCII code alphabet is D")
+elif x == 69:
+    print("ASCII code alphabet is E")
+elif x == 70:
+    print("ASCII code alphabet is F")
+elif x == 71:
+    print("ASCII code alphabet is G")
+elif x == 72:
+    print("ASCII code alphabet is H")
+elif x == 73:
+    print("ASCII code alphabet is I")
+elif x == 74:
+    print("ASCII code alphabet is J")
+elif x == 75:
+    print("ASCII code alphabet is K")
+elif x == 76:
+    print("ASCII code alphabet is L")
+elif x == 77:
+    print("ASCII code alphabet is M")
+elif x == 78:
+    print("ASCII code alphabet is N")
+elif x == 79:
+    print("ASCII code alphabet is O")
+elif x == 80:
+    print("ASCII code alphabet is P")
+elif x == 81:
+    print("ASCII code alphabet is Q")
+elif x == 82:
+    print("ASCII code alphabet is R")
+elif x == 83:
+    print("ASCII code alphabet is S")
+elif x == 84:
+    print("ASCII code alphabet is T")
+elif x == 85:
+    print("ASCII code alphabet is U")
+elif x == 86:
+    print("ASCII code alphabet is V")
+elif x == 87:
+    print("ASCII code alphabet is W")
+elif x == 88:
+    print("ASCII code alphabet is X")
+elif x == 89:
+    print("ASCII code alphabet is Y")
+elif x == 90:
+    print("ASCII code alphabet is Z")
+else:
+    print("Enter a valid input!")
