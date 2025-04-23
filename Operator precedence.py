adf6e8fc35eb044788648e362e28a80f630677ba
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-23 12:03:36 +0530

    Operator precedence

---
+++ Operator precedence.py
@@ -0,0 +1,12 @@
+a = 2
+b = 4
+c = 6
+d = 8
+e = 10
+x = a*b**c/d
+print("Value of a*b**c/d is: ",x)
+
+name = 'Alex'
+age = 0
+if name=='Alex' or name=='John' and age>=2:
+    print("Hello world!..!")
