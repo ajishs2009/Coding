d1aedbd2b531ffebbedaee1c9683b862d6ebe6d3
ajishs2009 <ajishsurender2009@gmail.com>
2025-04-24 12:31:26 +0530

    check age

---
+++ check age.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age"))
+if age>10 and age<20:
+    print('You are allowed in the category of 10-20')
+elif age<10 and age>20:
+    print("You aren't allowed")
---
+++ customise your ride.py
@@ -0,0 +1,16 @@
+print("Select your ride: ")
+print("1.Bike")
+print("2.Car")
+choice = input("Enter your choice")
+if choice==1:
+    print("Choose the options")
+    print("1.Scooty")
+    print("2.Scooter")
+    choice_2 = input("Enter your choice")
+    if choice_2==1:
+     print("You have selected scooty")
+    else:
+     print("You have selected scooter")
+
+elif (choice)==2:
+  print()
