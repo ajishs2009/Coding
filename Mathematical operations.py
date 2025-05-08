83ef7fab18f29ac0a082736ef46cbaa88f4f7598
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-08 10:06:57 +0530

    math operation

---
+++ Mathematical operations.py
@@ -0,0 +1,8 @@
+import math
+print("The floor and ceiling value of 23.56 will be: " + str(math.floor(23.56)) + ',' + str(math.ceil(23.56)))
+x = 10
+y = -15
+print("value of x after copying the sign of y is: ")
+print(math.copysign(x,y))
+print('Absolute value of -9 and 5 is: ' + str(math.fabs(-9)) + ',' + str(math.fabs(5)))
+print('The gcd of 26 and 36 is: ' + str(math.gcd(26,36)))
