89c83f064928281ff1f6f46cce1ebc0a0acd280d
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-08 10:36:49 +0530

    trigonometry

---
+++ trigonometry.py
@@ -0,0 +1,13 @@
+import math
+angle = 60
+angle_rad = math.radians(angle)
+sine = math.sin(angle_rad)
+print("Sine of 60 is: ",sine)
+angle = 30
+angle_rad = math.radians(angle)
+Cos = math.cos(angle_rad)
+print("Cos of 60 is: ",Cos)
+angle = 45
+angle_rad = math.radians(angle)
+tan = math.tan(angle_rad)
+print("Tan of 60 is: ",tan)
