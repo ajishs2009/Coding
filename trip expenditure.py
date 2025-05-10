f0ead5dc8c579eec324780037b20810616506c80
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-10 11:34:04 +0530

    trip expenditure

---
+++ trip expenditure.py
@@ -0,0 +1,23 @@
+def hotel_cost(night):
+    return 140*night
+def plane_cost(city):
+    if 'Charlotte'==city:
+        return 183
+    elif 'Tampa'==city:
+        return 220
+    elif 'Pittsburg'==city:
+        return 222
+    elif 'Los Angeles'==city:
+        return 475
+def rent_car(days):
+    if days>=7:
+        return 40*days - 50
+    elif days>=3:
+        return 40*days - 20
+    else:
+        return 40*days
+def trip_cost(city, days, spending_cost):
+    return rent_car(days) + plane_cost(city) + hotel_cost(days) + spending_cost
+print("Cost of rent car:", rent_car(5))
+print("Cost of hotel:", hotel_cost(5))
+print("Cost of plane ticket:", plane_cost("Los Angeles"))
