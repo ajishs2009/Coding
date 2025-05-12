0fadf31280949e768b72cdaf29dc720adb19f662
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-12 10:33:16 +0530

    tuple

---
+++ Tuple operation.py
@@ -0,0 +1,13 @@
+tuple = ('Tuple',True,3.2,1)
+print(tuple)
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuplex = (60,50,40,30,20,60)
+print(tuplex.count(60))
+tuplex = (2,4,3,5,6,7,8,1,9)
+slice = tuplex[3:5]
+print(slice)
+slice  = tuplex[:6]
+print(slice)
