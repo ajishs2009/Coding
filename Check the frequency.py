5c993b1b878f2b047835d2d343d91f250335d02b
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-16 10:23:23 +0530

    check the frequency

--- Check the frequency.py
+++ Check the frequency.py
@@ -1,4 +1,7 @@
 test_dict = {'Codingal':2, 'is':2,'best':2,'for':2,'coding':1}
 res = 0
 K = 2
-if 
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res+1
+print('Frequency is: ',res)
