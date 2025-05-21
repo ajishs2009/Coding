11c3e58e489e6c0cf98041374cb61778e9ecb91b
ajishs2009 <ajishsurender2009@gmail.com>
2025-05-21 09:53:54 +0530

    computer price

---
+++ computer price.py
@@ -0,0 +1,13 @@
+class Computer:
+    def __init__(self):
+        self.__max_price = 900
+    def sell(self):
+        print('Selling price:{}'.format(self.__max_price))
+    def setmaxprice(self,price):
+        self.__max_price = price
+c = Computer()
+c.sell()
+c.__max_price = 1000
+c.sell()
+c.setmaxprice(1000)
+c.sell
