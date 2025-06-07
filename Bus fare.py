f84063709a688f66ed03ba8dfdd6d7f796170cbc
ajishs2009 <ajishsurender2009@gmail.com>
2025-06-07 10:18:33 +0530

    bus fare

--- Bus fare.py
+++ Bus fare.py
@@ -1,6 +1,17 @@
 class Vehicle:
-    def __init__(self,fare,seats):
-        self.fare = fare
-        self.seats = seats
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
     def fare(self):
-        print('Total fare is {}'.format(self.fare,self.seats))
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10 / 100
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
