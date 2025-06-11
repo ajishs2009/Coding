8d59b945db6ba13f0ccaabf836ed70f9285708d2
ajishs2009 <ajishsurender2009@gmail.com>
2025-06-11 11:20:28 +0530

    sprite collision

--- sprite collision.py
+++ sprite collision.py
@@ -4,7 +4,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 500,400
 MOVEMENT_SPEED = 5
 FONT_SIZE = 72
 pygame.init()
-background_image = pygame.transform.scale(pygame.image.load(''),(SCREEN_WIDTH, SCREEN_HEIGHT))
+background_image = pygame.transform.scale(pygame.image.load('bg.jpg'),(SCREEN_WIDTH, SCREEN_HEIGHT))
 font = pygame.font.SysFont('Times new Roman', FONT_SIZE)
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, color, height, width):
@@ -25,7 +25,8 @@ all_sprites.add(sprite_1)
 sprite_2 = Sprite(pygame.Color('red'),20,30)
 sprite_2.rect.x, sprite_1.rect.y = random.randint(0, SCREEN_WIDTH - sprite_1.rect.width), random.randint(0,SCREEN_HEIGHT - sprite_1.rect.height)
 all_sprites.add(sprite_2)
-running, won = True, Falseclock = pygame.time.Clock()
+running, won = True, False
+clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_x):
@@ -39,4 +40,16 @@ while running:
         if sprite_1.rect.colliderect(sprite_2.rect):
             all_sprites.remove(sprite_2)
             won = True
-    screen
+    screen.blit(background_image, (0, 0))
+    all_sprites.draw(screen)
+
+  # Display win message
+    if won:
+      win_text = font.render("You win!", True, pygame.Color('black'))
+      screen.blit(win_text, ((SCREEN_WIDTH - win_text.get_width()) // 2,
+                           (SCREEN_HEIGHT - win_text.get_height()) // 2))
+
+    pygame.display.flip()
+    clock.tick(90)
+
+pygame.quit()
