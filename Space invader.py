19b87369197f22bc8021828f431ba94e2ac4f610
ajishs2009 <ajishsurender2009@gmail.com>
2025-06-12 10:10:56 +0530

    space invader

--- Space invader.py
+++ Space invader.py
@@ -56,11 +56,58 @@ def game_over_text():
     screen.blit(over_text, (200,250))
 def player(x,y):
     screen.blit(playerImg, (x,y))
+def enemy(x,y,i):
+    screen.blit(enemyImg[i], (x,y))
 def fire_bullet(x,y):
     global bullet_state
-    bullet_stste = 'fire'
+    bullet_state = 'fire'
     screen.blit(bulletImg, (x+16,y+10))
 def isCollision(enemyX, enemyY, bulletX, bulletY):
     distance = math.sqrt(enemyX-bulletX)**2 + (enemyY-bulletY)**2
     return distance < COLLISION_DISTANCE
+running = True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background, (0,0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            if event.key == pygame.K_SPACE and bullet_state == 'ready':
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+            playerX_change = 0
+
+    playerX += playerX_change
+    playerX = max(0,min(playerX, SCREEN_WIDTH - 64))
+
+    for i in range(num_of_enemies):
+        if enemyY[i] > 340:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+                game_over_text()
+                break
+        
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <=0 or enemyX[i] >= SCREEN_WIDTH - 64:
+            enemyX_change[i] *= -1
+            enemyY[i] += enemyY_change[i]
+        
+        if isCollision(enemyX[i], enemyY[i], bulletX, bulletY):
+            bulletY = PLAYER_START_Y        
+            bullet_state = 'ready'
+            score_value += 1
+            enemyX[i] = random.randint(0, SCREEN_WIDTH - 64)
+            enemyY[i] = random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX)
+
+        enemy(enemyX[i], enemyY[i],i)
+
+
+
 
