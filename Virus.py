845d93b91e1685711d5e331dc174dd4538f328a6
ajishs2009 <ajishsurender2009@gmail.com>
2025-06-17 14:36:50 +0530

    text editor

--- Virus.py
+++ Virus.py
@@ -5,6 +5,12 @@ root.geometry('200x200')
 
 def msg():
     messagebox.showwarning('Alert', 'Virus found!')
+    messagebox.showinfo('Info', 'This virus is not halmful!')
+    messagebox.showerror('Error', 'Syntax Error!')
+    messagebox.askquestion('You know?', 'Yes or no')
+    messagebox.askokcancel('Cancel', 'Yes!')
+    messagebox.askyesno('Yes?', 'No!')
+    messagebox.askretrycancel('Alert', 'Virus found!')
 
 button = Button(root, text='Scan for virus', command=msg)
 button.place(x=40, y=80)
